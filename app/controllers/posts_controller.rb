02c89dcb99b12ab7b68f2c82b903e061eb684774
teasacura <teasacuraster@gmail.com>
2018-05-23 18:02:55 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,12 +5,17 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.new
   end
 
   def update
     @post.update(post_params)
-
-    redirect_to post_path(@post)
+    if @post.valid?
+      @post.save
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
 
   private
